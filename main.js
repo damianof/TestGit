0dfaeae9199b63e87116693e4a44bb2141ae2fd4
Damiano Fusco <damianofusco@gmail.com>
2016-04-29 12:24:04 -0400

    Commit to alpha

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /*global process, require*/
 'use strict';
+const git = require('git-rev');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3333;
 
-const _branchName = 'master';
+let _branchName = 'XYZ';
 const _lastUpdated = 'April 29th at 12:16pm';
 
 const server = http.createServer((req, res) => {
@@ -14,6 +15,13 @@ const server = http.createServer((req, res) => {
 	res.end('Code is from branch: ' + _branchName + '\nlast updated on: ' + _lastUpdated);
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/ for branch ` + _branchName);
-});
+git.branch(function (str) {
+	console.log('branch', str)
+	_branchName = str;
+	
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/ for branch ` + _branchName);
+	});
+})
+
+
