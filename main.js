294f21505ebeb59b8895c044d47f93fec11b0b02
Damiano Fusco <damianofusco@gmail.com>
2016-04-29 15:10:07 -0400

    My project is ready

--- main.js
+++ main.js
@@ -6,24 +6,20 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3333;
 
-let _branchName = 'XYZ';
-const _lastUpdated = 'April 29th at 14:52pm';
-
 fs.readFile('branch.txt', 'utf8', (err, str) => {
 	if (err){
 		throw err;
 	}
 	
 	console.log('branch', str);
-	_branchName = str;
 	
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/plain');
-		res.end('Code is from branch: ' + _branchName + '\nlast updated on: ' + _lastUpdated);
+		res.end('Code is from branch: ' + str);
 	});
 	
 	server.listen(port, hostname, () => {
-		console.log(`Server running at http://${hostname}:${port}/ for branch ` + _branchName);
+		console.log(`Server running at http://${hostname}:${port}/ for branch ` + str);
 	});
 });
